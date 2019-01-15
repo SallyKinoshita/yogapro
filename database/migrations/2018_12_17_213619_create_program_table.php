986156beb1d95e53bc5b43fb28e855ee13bd48eb
Hitomi Sally Kinoshita <sunnysunnysally@gmail.com>
2019-01-15 22:50:55 +0900

    テーブル名を修正

--- database/migrations/2018_12_17_213619_create_program_table.php
+++ database/migrations/2018_12_17_213619_create_program_table.php
@@ -13,7 +13,7 @@ class CreateProgramTable extends Migration
      */
     public function up()
     {
-        Schema::create('program', function (Blueprint $table) {
+        Schema::create('programs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
             $table->timestamps();
@@ -28,6 +28,6 @@ class CreateProgramTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('program');
+        Schema::dropIfExists('programs');
     }
 }
